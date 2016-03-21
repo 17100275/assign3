f99d553da9075b8147c6ea7a8af57ec396acff4a
a17100275 <17100275@lums.edu.pk>
2016-03-21 16:52:09 +0000

    done assignent

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,13 @@
 # Add a declarative step here for populating the DB with movies.
 
+
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
+#  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +16,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  temp = page.body =~/#{e1}.*#{e2}/m 
+  assert !temp.nil?
+  
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +29,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  rating_list.delete!("\"")
+  rating_list.split(",").each do |temp|
+    if uncheck.nil?
+      check("ratings["+temp.strip+"]")
+    else
+      uncheck("ratings["+temp.strip+"]")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+  # flunk "Unimplemented"
+  page.all("table#movies tbody tr").count.should == Movie.count
+
 end
